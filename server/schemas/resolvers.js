150f4d6efe202a9ec3fc0f64fee8f2e2345a6779
Becca Wyland <rwyland@uwalumni.com>
2021-07-28 23:49:59 -0500

    write query resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,16 @@
+
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await user.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+
+                return userData;
+            }
         }
-      }
-  };
+    }
+};
   
-  module.exports = resolvers;
+module.exports = resolvers;
