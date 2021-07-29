4acee633cfc3f056370d172598745ea6c4e3df7e
Becca Wyland <rwyland@uwalumni.com>
2021-07-29 00:34:45 -0500

    start writing mutation resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,6 +10,13 @@ const resolvers = {
                 return userData;
             }
         }
+    },
+
+    Mutation: {
+        login: ,
+        addUser:,
+        saveBook:,
+        removeBook:
     }
 };
   
