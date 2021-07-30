2ac2240e58d20e6a486b6fc1881c6a513c3faf6e
Becca Wyland <rwyland@uwalumni.com>
2021-07-29 23:55:23 -0500

    write REMOVE_BOOK

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -50,8 +50,20 @@ export const SAVE_BOOK = gql`
 
 export const REMOVE_BOOK = gql`
     {
-        mutation {
-            
+        mutation removeBook($bookId: ID!) {
+            removeBook(bookId: $bookId) {
+                _id
+                username
+                email
+                savedBooks {
+                    bookId
+                    authors
+                    image
+                    description
+                    title
+                    link
+                }
+            }
         }
     }
 `;
