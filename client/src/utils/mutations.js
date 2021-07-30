0b1bbaf1611a8dc16c36fbbea1ccf543be36da06
Becca Wyland <rwyland@uwalumni.com>
2021-07-29 23:52:45 -0500

    write ADD_USER

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -16,8 +16,14 @@ export const LOGIN_USER = gql`
 
 export const ADD_USER = gql`
     {
-        mutation {
-            
+        mutation addUser($username: String!, $email: String!, $password: String!) {
+            addUser(username: $username, email: $email, password: $password) {
+                token
+                user {
+                    _id
+                    username
+                }
+            }
         }
     }
 `;
