3b77f441e610315c533b444f874e52894622d3fb
Becca Wyland <rwyland@uwalumni.com>
2021-07-29 23:51:33 -0500

    write LOGIN_USER

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,8 +2,14 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
     {
-        mutation {
-            
+        mutation login($email: String!, $password: String!) {
+            login(email: $email, password: $password) {
+                token
+                user {
+                    _id
+                    username
+                }
+            }
         }
     }
 `;
