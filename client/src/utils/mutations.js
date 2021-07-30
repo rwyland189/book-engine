2c1ffeedc59dc075d89dffeebfe4f9f14a08ba5e
Becca Wyland <rwyland@uwalumni.com>
2021-07-29 23:50:05 -0500

    start writing mutations.js on client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    {
+        mutation {
+            
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    {
+        mutation {
+            
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    {
+        mutation {
+            
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    {
+        mutation {
+            
+        }
+    }
+`;
+
+
